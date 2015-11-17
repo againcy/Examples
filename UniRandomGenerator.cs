33abfecfab1f494728a70d3a34634b52fcdaf9e7
againcy <againde@gmail.com>
2015-11-17 16:52:06 +0900

    添加两个新的例子
    添加均匀随机数生成器和向量类

---
+++ UniRandomGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataMining_Assignment_4
+{
+    /// <summary>
+    /// 均匀随机数生成器
+    /// 生成从0到N-1之间的均匀随机数
+    /// </summary>
+    class UniRandomGenerator
+    {
+        private int N;//随机数个数
+        private int[] numList;//随机数表
+        private int cur;//记录当前取到第几个随机数
+        private Random rand;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="n">生成从0到n-1之间的均匀随机数</param>
+        public UniRandomGenerator(int n)
+        {
+            N = n;
+            numList = new int[n];
+            rand = new Random();
+            GenerateNewList();
+        }
+
+        /// <summary>
+        /// 生成新的随机数表
+        /// </summary>
+        private void GenerateNewList()
+        {
+            for (int i = 0; i < N; i++) numList[i] = i;
+            //对一个0到N-1顺序排列的数列做N次随机位置的两两交换以打乱顺序
+            for (int i = 0; i < N; i++)
+            {
+                int i1 = rand.Next() % N;
+                int i2 = rand.Next() % N;
+                var tmp = numList[i1];
+                numList[i1] = numList[i2];
+                numList[i2] = tmp;
+            }
+            cur = 0;
+        }
+
+        /// <summary>
+        /// 获取下一个随机数
+        /// </summary>
+        /// <returns>一个随机0到N-1之间的整数</returns>
+        public int GetNext()
+        {
+            if (cur==N)
+            {
+                GenerateNewList();
+            }
+            int result = numList[cur];
+            cur++;
+            return result;
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataMining_Assignment_4
+{
+    class Vector
+    {
+        public double[] value;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="v">向量</param>
+        public Vector(double[] v)
+        {
+            value = v;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="v">向量</param>
+        public Vector(Array v)
+        {
+            value = new double[v.Length];
+            for (int i = 0; i < v.Length; i++) value[i] = Convert.ToDouble( v.GetValue(i));
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="v">向量</param>
+        public Vector(Vector v)
+        {
+            this.value = new double[v.value.Length];
+            for (int i = 0; i < value.Length; i++) value[i] = v.value[i];
+        }
+
+        /// <summary>
+        /// 构造函数，创建n维0向量
+        /// </summary>
+        /// <param name="n">向量维数</param>
+        public Vector(int n)
+        {
+            value = new double[n];
+            value.Initialize();
+        }
+
+        /// <summary>
+        /// 向量内积
+        /// </summary>
+        /// <param name="v1">向量1</param>
+        /// <param name="v2">向量2</param>
+        /// <returns>内积</returns>
+        public static double operator *(Vector v1, Vector v2)
+        {
+
+            double innerProduction = 0;
+            try
+            {
+                for (int i = 0; i < v1.value.Length; i++) innerProduction += v1.value[i] * v2.value[i];
+            }
+            catch
+            {
+                Console.WriteLine("Error at inner production");
+            }
+            return innerProduction;
+        }
+
+        /// <summary>
+        /// 向量与实数乘积
+        /// </summary>
+        /// <param name="num">实数</param>
+        /// <param name="v">向量</param>
+        /// <returns>乘积</returns>
+        public static Vector operator *(double num, Vector v)
+        {
+            try
+            {
+                for (int i = 0; i < v.value.Length; i++) v.value[i] *= num;
+            }
+            catch
+            {
+                Console.WriteLine("Error at multiply");
+            }
+            return v;
+        }
+
+        /// <summary>
+        /// 向量减法
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            Vector result = new Vector(v1);
+            try
+            {
+                for (int i = 0; i < result.value.Length; i++) result.value[i] -= v2.value[i];
+            }
+            catch
+            {
+                Console.WriteLine("Error at minus");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 向量加法
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector result = new Vector(v1);
+            try
+            {
+                for (int i = 0; i < result.value.Length; i++) result.value[i] += v2.value[i];
+            }
+            catch
+            {
+                Console.WriteLine("Error at plus");
+            }
+            return result;
+        }
+
+
+    }
+}
